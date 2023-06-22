4bf85503d8a88561f299c958f3e018130c69f69d
Colin Huber <cbhuber@gmail.com>
2023-06-21 22:07:49 -0600

    feat: upgrade to date counter: slider step and text counter.

--- src/App.js
+++ src/App.js
@@ -16,26 +16,50 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => (s = s - 1))}>-</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* <button onClick={() => setStep((s) => (s = s - 1))}>-</button> */}
         {`Step: ${step}`}
-        <button onClick={() => setStep((s) => (s = s + 1))}>+</button>
+        {/* <button onClick={() => setStep((s) => (s = s + 1))}>+</button> */}
       </div>
       <div>
         <button onClick={() => setCount((c) => (c = c - step))}>-</button>
-        {`Count: ${count}`}
+        {/* {`Count: ${count}`} */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => (c = c + step))}>+</button>
       </div>
-      <span>
-        {count === 0
-          ? "Today is "
-          : count < 0
-          ? `${Math.abs(count)} day(s) ago was `
-          : `${count} day(s) from now is `}
-      </span>
-      <span>{date.toDateString()}</span>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count < 0
+            ? `${Math.abs(count)} day(s) ago was `
+            : `${count} day(s) from now is `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
